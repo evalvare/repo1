319c3f7f862f24ff4f64a3f72060dc0a07dcafe0
evalvare <evalvare@LES005500.corp.capgemini.com>
2018-08-07 09:21:25 +0200

    clase estudiante creada el martes

---
+++ GitHubDemo/src/main/java/com/ealvarez/modelo/Estudiante.java
@@ -0,0 +1,15 @@
+package com.ealvarez.modelo;
+
+public class Estudiante {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
